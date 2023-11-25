2feaf49c94a479ca5f9593f3691e506914faabfc
Alihan Soykal <alihansoykal@gmail.com>
2023-11-25 21:53:08 +0300

    configure dev env

---
+++ lib/product/init/config/dev_env.g.dart
@@ -0,0 +1,63 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dev_env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _DevEnv {
+  static const List<int> _enviedkey_baseUrl = <int>[
+    2090808991,
+    2860091394,
+    1575985990,
+    3598303364,
+    121121009,
+    3469059881,
+    2803128338,
+    4132023114,
+    708495727,
+    3232294950,
+    79541442,
+    2364278099,
+    1325144506,
+    3387354938,
+    3231264942,
+    208506860,
+    3777460994,
+    396403740,
+    1638890908,
+    3148807406,
+  ];
+
+  static const List<int> _envieddata_baseUrl = <int>[
+    2090809079,
+    2860091510,
+    1575985970,
+    3598303476,
+    121120898,
+    3469059859,
+    2803128381,
+    4132023141,
+    708495627,
+    3232294985,
+    79541413,
+    2364278141,
+    1325144537,
+    3387354975,
+    3231264961,
+    208506819,
+    3777461091,
+    396403820,
+    1638890997,
+    3148807361,
+  ];
+
+  static final String _baseUrl = String.fromCharCodes(List<int>.generate(
+    _envieddata_baseUrl.length,
+    (int i) => i,
+    growable: false,
+  ).map((int i) => _envieddata_baseUrl[i] ^ _enviedkey_baseUrl[i]));
+}
