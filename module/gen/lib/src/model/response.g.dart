5adc268dd4d0cdcf6010ade0be605bbd0a9aa98a
Alihan Soykal <alihansoykal@gmail.com>
2023-11-27 21:20:12 +0300

    create response model

--- module/gen/lib/src/model/response.g.dart
+++ module/gen/lib/src/model/response.g.dart
@@ -1,18 +1,17 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'sub_breed.dart';
+part of 'response.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-SubBreed _$SubBreedFromJson(Map<String, dynamic> json) => SubBreed(
-      message:
-          (json['message'] as List<dynamic>?)?.map((e) => e as String).toList(),
+Response _$ResponseFromJson(Map<String, dynamic> json) => Response(
+      message: json['message'] as String?,
       status: json['status'] as String?,
     );
 
-Map<String, dynamic> _$SubBreedToJson(SubBreed instance) => <String, dynamic>{
+Map<String, dynamic> _$ResponseToJson(Response instance) => <String, dynamic>{
       'message': instance.message,
       'status': instance.status,
     };
